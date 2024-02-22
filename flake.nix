04fb08e38ef3977f5915188b39e09679c73341b1
clo4 <git@clo4.net>
2024-02-23 00:05:50 +1100

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.alejandra;
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          nodePackages_latest.prettier
+        ];
+      };
+    });
+}
