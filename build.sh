6e0f5eb0ad065f540618cf95f4e0e69ca7a4d07a
clo4 <git@clo4.net>
2024-06-11 02:05:56 +1000

    Update build script for compat with pack version 45
    This is currently not gated but will be in a future version.
    Any packs with singular 'function' or 'advancement' directories will
    have them duplicated for compatibility with older versions, so the min
    version doesn't have to be bumped and it isn't any more effort to
    maintain.

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Requires: zip, jq, (recent-ish) bash
 
 set -e
-
-# Requires: zip, jq
+shopt -s globstar nullglob
 
 packs=$(find . -name pack.mcmeta)
 
@@ -11,14 +12,22 @@ if [ -d dist ]; then
 fi
 mkdir -p dist
 
-wd=$(pwd)
+if [ -d build ]; then
+  rm -rf build/*
+fi
+mkdir -p build
 
 for pack in $packs; do
-  cd $wd
+  cp -R $(basename $(dirname $pack)) build
+done
 
+wd=$(pwd)
+
+for dir in $(ls build); do
+  cd $wd/build
   # This can be done using the horrible bash string manipulation, but
   # I dislike that on a spiritual level. I feel it in my whole body.
-  dir=$(basename $(dirname $pack))
+  # dir=$(basename $(dirname $pack))
   name=$(sed 's/-/_/g' <<< $dir)
 
   # Don't know what could trigger this - maybe if in the future I
@@ -30,18 +39,26 @@ for pack in $packs; do
 
   echo "Building $dir"
 
-  packversion=$(jq -r .version $dir/version.json)
-  mc0=$(jq -r .minecraft[0] $dir/version.json)
-  mc1=$(jq -r .minecraft[1] $dir/version.json)
+  # zip really wants to include the dir heirarchy, so we have to cd into the directory
+  cd $dir
+
+  packversion=$(jq -r .version version.json)
+  mc0=$(jq -r '.minecraft[0]' version.json)
+  mc1=$(jq -r '.minecraft[1]' version.json)
 
   if ! git diff --quiet --exit-code $dir; then
     name+="__DIRTY__DO_NOT_RELEASE"
   fi
 
-  # zip really wants to include the dir heirarchy, so we have to cd into the directory
-  cd $dir
+  cp $wd/LICENSE .
 
-  cp ../LICENSE .
+  # TODO: Gate on whether pack version 45 is below max and above min
+  # This duplicates the function/advancement directories, which makes
+  # the structure compatible with older versions
+  compat_dirs=$(echo **/function **/advancement)
+  for compat_dir in $compat_dirs; do
+    cp -R $compat_dir ${compat_dir}s
+  done
 
   # Specifying the files to include means I can put assets in the folders in the future
   # and not worry about them being included in the final zips.
