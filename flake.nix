39b36b1463f2a146184b22022b74a0335b2a6fb1
clo4 <git@clo4.net>
2024-10-24 12:27:06 +1100

    Use nixfmt and add envrc

--- flake.nix
+++ flake.nix
@@ -8,20 +8,26 @@
     };
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-utils,
-    mcmeta-summary,
-  }:
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-utils,
+      mcmeta-summary,
+    }:
     {
-      overlays.default = final: prev: let
-        builder = final.callPackage (import ./nix/builder.nix) {
-          minecraftVersionsSummary = builtins.fromJSON (builtins.readFile "${mcmeta-summary}/versions/data.json");
+      overlays.default =
+        final: prev:
+        let
+          builder = final.callPackage (import ./nix/builder.nix) {
+            minecraftVersionsSummary = builtins.fromJSON (
+              builtins.readFile "${mcmeta-summary}/versions/data.json"
+            );
+          };
+        in
+        {
+          inherit (builder) buildDataPack joinDataPacks;
         };
-      in {
-        inherit (builder) buildDataPack joinDataPacks;
-      };
       templates = {
         default = {
           description = "Single data pack in the root of the repository";
@@ -33,37 +39,41 @@
         };
       };
     }
-    // (flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system}.extend self.overlays.default;
-    in rec {
-      formatter = pkgs.alejandra;
-      devShells.default = pkgs.mkShell {
-        packages = [
-          pkgs.nixd
-          pkgs.deno
-          pkgs.zip
-          pkgs.unzip
-          formatter
-        ];
-      };
+    // (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system}.extend self.overlays.default;
+      in
+      rec {
+        formatter = pkgs.nixfmt-rfc-style;
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nixd
+            pkgs.deno
+            pkgs.zip
+            pkgs.unzip
+            formatter
+          ];
+        };
 
-      packages = rec {
-        afk = pkgs.callPackage ./afk {};
-        afk-sleep = pkgs.callPackage ./afk-sleep {};
-        afk-message = pkgs.callPackage ./afk-message {};
-        afk-dim-names = pkgs.callPackage ./afk-dim-names {};
-        pause-day-cycle = pkgs.callPackage ./pause-day-cycle {};
-        chickenfix = pkgs.callPackage ./chickenfix {};
+        packages = rec {
+          afk = pkgs.callPackage ./afk { };
+          afk-sleep = pkgs.callPackage ./afk-sleep { };
+          afk-message = pkgs.callPackage ./afk-message { };
+          afk-dim-names = pkgs.callPackage ./afk-dim-names { };
+          pause-day-cycle = pkgs.callPackage ./pause-day-cycle { };
+          chickenfix = pkgs.callPackage ./chickenfix { };
 
-        all = pkgs.joinDataPacks "clo4-datapacks" [
-          afk
-          afk-sleep
-          afk-message
-          afk-dim-names
-          pause-day-cycle
-          chickenfix
-        ];
-        default = all;
-      };
-    }));
+          all = pkgs.joinDataPacks "clo4-datapacks" [
+            afk
+            afk-sleep
+            afk-message
+            afk-dim-names
+            pause-day-cycle
+            chickenfix
+          ];
+          default = all;
+        };
+      }
+    ));
 }
