6005f1fdfff62a668d3e76a15e94b73818c7c6d3
clo4 <git@clo4.net>
2024-11-18 21:34:34 +1100

    Remove compatibility preprocess step

--- afk/default.nix
+++ afk/default.nix
@@ -5,15 +5,5 @@ buildDataPack {
 
   src = ./.;
 
-  preprocess = ''
-    shopt -s nullglob globstar
-    pushd data
-    for dir in **/function **/advancement; do
-      cp -r $dir ''${dir}s
-    done
-    popd
-    touch **
-  '';
-
   include = [ "LICENSE" ];
 }
