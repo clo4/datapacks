53152f0202c08ddb2ee4e7292b595a2cc5ba6c3c
clo4 <git@clo4.net>
2024-11-23 01:04:11 +1100

    Add AFK Immunity

---
+++ afk-immunity/data/afk_immunity/function/away.mcfunction
@@ -0,0 +1,1 @@
+effect give @s resistance infinite 255 true
---
+++ afk-immunity/default.nix
@@ -0,0 +1,19 @@
+{ buildDataPack }:
+buildDataPack {
+  name = "afk-immunity";
+  version = "1.0.0";
+
+  src = ./.;
+
+  preprocess = ''
+    shopt -s nullglob globstar
+    pushd data
+    for dir in **/function **/advancement; do
+      cp -r $dir ''${dir}s
+    done
+    popd
+    touch **
+  '';
+
+  include = [ "LICENSE" ];
+}
